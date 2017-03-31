55a69d27fb90a130385bdedd27394b5fc60889e6
Student <student@mbp-121-harris.home>
2017-03-30 22:35:09 -0400

    added background and koala sprite. Koala able to move horizontally by touching screen.

--- platform man/GameScene.swift
+++ platform man/GameScene.swift
@@ -10,10 +10,49 @@ import SpriteKit
 
 
 class GameScene: SKScene {
-
+ let koala = SKSpriteNode(imageNamed: "koala") // we want to be able to manipulate the koala for the game
+    
     override func didMove(to view: SKView){
-        backgroundColor = SKColor.white
+        backgroundColor = SKColor.black
+        let background = SKSpriteNode(imageNamed: "forest")
+        background.position = CGPoint(x:size.width/2, y:size.height/2)//anchor image middle of screen horizontally and vertically as well
+        background.size = self.frame.size
+        background.zPosition = -1
+        addChild(background)
+        koala.setScale(2)
+        koala.position = CGPoint(x: size.width/2, y: 300)
+        addChild(koala)
+    
+        
 }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {
+            return
+        }
+        //get location of first touch
+        let touchlocation = touch.location(in: self)
+        print(touchlocation.x)
+        print(touchlocation.y)
+        //moves koala horizontally
+        let destination = CGPoint(x: touchlocation.x, y: koala.position.y)
+        
+        let actionMove = SKAction.move(to: destination, duration: 2)
+        koala.run(actionMove)
+    }
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {
+            return
+        }
+         //get location of first touch
+        let touchlocation = touch.location(in: self)
+        let destination = CGPoint(x: touchlocation.x, y: koala.position.y)
+        
+        let actionMove = SKAction.move(to: destination, duration: 2)
+        koala.run(actionMove)
+
+       
+
+    }
 }
 
 
